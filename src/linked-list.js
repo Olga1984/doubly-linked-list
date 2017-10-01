64088f19c6959af9d0541287dded948360717552
Olga1984 <noa84@tut.by>
2017-10-01 21:32:51 +0300

    add new method Node and changed at(position), deleteAt and reverse methods

--- src/linked-list.js
+++ src/linked-list.js
@@ -2,21 +2,23 @@ const Node = require('./node');
 
 class LinkedList {
     constructor() {
-        //assign 0 to this.length        
+        //assign 0 to this.length
+        this._head = 0;
+        this._tail = 0;
         this.length = 0;
     }
     append(data) {
         //should assign any nodes to this._head and this._tail if list is empty
         //should add new data to the end of list    
-        var node = new Node(data);
-        if (!this._head){
-            this._head = node;
-        }else{
-            this._tail.next = node;
-            node.prev = this._tail;
+        var current = new Node(data);
+        if (!this._head) {
+            this._head = current;
+        } else {
+            this._tail.next = current;
+            current.prev = this._tail;
         }
 
-        this._tail = node;
+        this._tail = current;
         this.length += 1;
 
         return this;
@@ -31,34 +33,42 @@ class LinkedList {
     }
     at(position) {
         //should return Node.data by index
-        var i = 0;
+        /**/
+        return this.Node(position).data;
+    }
+    Node(position) {
+        if (position < 0 || position >= this.length) {
+            return null;
+        }
         var current = this._head;
-        while (i < this.length) {
-            if(i == position){ return current.data;}
-            else{current = current.next;
-                i++;}
-        }        
+        for (var i = 0; i < position; i++) {
+            current = current.next;
+        }
+        return current;
     }
     insertAt(index, data) {
         //should insert data by index
-         var node = new Node(data);
+        var current = new Node(data);
         if (index == 0) {
-            node.next = this._head;
-            this._head.prev = node;
-            this._head = node;
+            current.next = this._head;
+            this._head.prev = current;
+            this._head = current;
         } else if (index < this.length && index > 0) {
             var count = 0;
             var insert = this._head;
-            while (count < index) { insert = insert.next; count++; }
+            while (count < index) {
+                insert = insert.next;
+                count++;
+            }
             this._head = insert;
             this._tail = insert.next;
-            node.prev = insert;
-            insert.next = node;
-            node.next = insert.next.next;
-            insert.next.next = node;
-            insert = node;
-        } else { 
-            return this; 
+            current.prev = insert;
+            insert.next = current;
+            current.next = insert.next.next;
+            insert.next.next = current;
+            insert = current;
+        } else {
+            return this;
         }
     }
     isEmpty() {
@@ -72,68 +82,54 @@ class LinkedList {
 
     clear() {
         //should clear the list        
-        var node = new Node();
-        this._head = node;
-        this._tail = node;
+        var current = new Node();
+        this._head = current;
+        this._tail = current;
         this.length = 0;
         return this;
     }
     deleteAt(index) {
         //should delete element by index
-        if (index > this.length - 1 || index < 0){
-            return null;
-        }
-        var node = this._head;
-        var i = 0;
-        if(index == 0){
-            this._head = node.next;
-            if(this._head = null){
-                this._tail = null;
+        if (index === 0) {
+            if (this.length === 1) {
+                this.clear();
+            } else {
+                this._head.next.prev = this._head.prev;
             }
-            else{
-                this._head.prev = null;
-            }
-        } else if (index ==this.length -1) {
-            node = this._tail;
-            this._tail = node.prev;
-            this._tail.next = null;
+        } else if (index === this.length - 1) {
+            this._tail.prev.next = this._tail.next;
         } else {
-            while(i++ < index) {
-                node = node.next;
-            }
-            node.prev.next = node.next;
-            node.next.prev = node.prev;
+            var current = this.Node(index);
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
         }
-        this.length--;
+        this.length -= 1;
         return this;
-        
     }
-
     reverse() {
-        //should reverse the list
-        var count = 0;
-        var len = this.length-1;
-        var head = this._head;
-        var tail = this._tail;
-        while(count < len) {
-            var reverse = tail.data;
-            tail.data = head.data;
-            head.data = reverse;
-            tail = tail.prev;
-            head = head.next;
-            count ++;
-            len--;
+        //should reverse the list       
+        var current = this._head;
+        for (var i = 0; i < this.length; i++) {
+            var tmp = current.next;
+            current.next = current.prev;
+            current.prev = tmp;
+            current = tmp;
         }
+        var head = this._head;
+        this._head = this._tail;
+        this._tail = head;
+
         return this;
     }
-
     indexOf(data) {
         //should return index of element if data is found
-        var ind = 0;            
+        var ind = 0;
         var current = this._head;
         while (ind < this.length) {
-            if (current.data == data){return ind;}
-            current = current.next;            
+            if (current.data == data) {
+                return ind;
+            }
+            current = current.next;
             ind += 1;
         }
         return -1;
